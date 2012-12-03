a0cd4768515ba254d0042313e0c1a4f0fead5412
Akshay Agrawal <akshayagrawal18@gmail.com>
2012-12-03 23:45:57 +0530

    Update detail1.php

--- detail1.php
+++ detail1.php
@@ -41,16 +41,25 @@
   $find = strtoupper($find);
   $find = strip_tags($find);
   $find = trim ($find);
-
+  echo"<table border=0 width=50% cellpadding=0 cellspacing=0 bgcolor=black>";
+  echo"<tr><td><font color=white face=arial size=5>Name</td>
+  <td><font color=white face=arial size=5>Address</td>
+  <td><font color=white face=arial size=5>Contact No</td>
+  <td><font color=white face=arial size=5>Designation</td>
+  </tr>";
   $query = mysql_query("SELECT * FROM contact1 WHERE upper(category) LIKE'%$find%'");
   while($result = mysql_fetch_array($query)){
-  echo $result['name'];
-  echo $result['Address'];
-  echo $result['ContactNO'];
-  echo $result['Designation'];
-  echo "<br>";
+  $a1 = $result['name'];
+  $c2 = $result['Address'];
+  $i2 = $result['ContactNO'];
+  $p2 = $result['Designation'];
+  echo"<tr><td><font color=white face=arial size=5>$a1</td>
+  <td><font color=white face=arial size=5>$c2</td>
+  <td><font color=white face=arial size=5>$i2</td>
+  <td><font color=white face=arial size=5>$p2</td>
+  </tr>";
   }
-
+    echo"</table>";
   $matches=mysql_num_rows($query);
   if ($matches == 0){
   echo "Sorry, we can not find an entry to match your query<br><br>";
